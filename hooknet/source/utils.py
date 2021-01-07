426441f578d49267266f2335a4d567d1ff25f004
martvanrijthoven <mart.vanrijthoven@gmail.com>
2021-01-07 19:56:08 +0100

    weights

--- hooknet/source/utils.py
+++ hooknet/source/utils.py
@@ -1,12 +1,14 @@
 import numpy as np
 from experiment.callbacks import TrainCallback
 
+def clean_weights(masks):
+    return np.clip(np.sum(masks, axis=-1), 0, 1)
 
 class HookNetReshape(TrainCallback):
     def __init__(self, multi_loss=False):
         self._multi_loss = multi_loss
 
-    def __call__(self, x_batch: np.ndarray, y_batch: np.ndarray):
+    def __call__(self, x_batch: np.ndarray, y_batch: np.ndarray, sample_weight):
         x_list_batch = [[], []]
         y_list_batch = [[], []]
 
@@ -25,6 +27,10 @@ class HookNetReshape(TrainCallback):
         y_list_batch[0] = np.array(y_list_batch[0])
         y_list_batch[1] = np.array(y_list_batch[1])
         if self._multi_loss:
-            return x_list_batch, y_list_batch
+            return (
+                x_list_batch,
+                y_list_batch,
+                [clean_weights(y_list_batch[0]), clean_weights(y_list_batch[1])],
+            )
 
-        return x_list_batch, y_list_batch[0]
+        return x_list_batch, y_list_batch[0], clean_weights(y_list_batch[0])
