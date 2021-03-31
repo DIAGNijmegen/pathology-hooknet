11b34a860124e799422b46e2aa38fb3bdc3cb5da
martvanrijthoven <mart.vanrijthoven@gmail.com>
2021-03-31 10:00:20 +0200

    fix syntax

--- hooknet/source/utils.py
+++ hooknet/source/utils.py
@@ -6,6 +6,8 @@ def clean_weights(masks):
 class HookNetReshape():
     def __init__(self, multi_loss=False, x_as_np=True, y_as_np=True):
         self._multi_loss = multi_loss
+        self._x_as_np = x_as_np
+        self._y_as_np = y_as_np
 
     def __call__(self, x: np.ndarray, y: np.ndarray, sample_weight=None):
         x_list_batch = [[], []]
@@ -21,11 +23,11 @@ class HookNetReshape():
             for idx, (key, value) in enumerate(batch_sample.items()):
                 y_list_batch[idx].append(value)
 
-        if x_as_np:
+        if self._x_as_np:
             x_list_batch[0] = np.array(x_list_batch[0])
             x_list_batch[1] = np.array(x_list_batch[1])
         
-        if y_as_np:
+        if self._y_as_np:
             y_list_batch[0] = np.array(y_list_batch[0])
             y_list_batch[1] = np.array(y_list_batch[1])
         
