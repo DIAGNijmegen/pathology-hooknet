831daff9a3177b770d626f3c1e71b93d58bb53c8
martvanrijthoven <mart.vanrijthoven@gmail.com>
2021-09-29 19:50:42 +0200

    update docker #9

--- setup.py
+++ setup.py
@@ -2,18 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="hooknet",
-    version="0.0.2",
+    version="0.0.3",
     author="Mart van Rijthoven",
     author_email="mart.vanrijthoven@gmail.com",
     packages=find_packages(exclude=("tests", "notebooks", "scripts", "os-level-virtualization", "docs")),
     url="http://pypi.python.org/pypi/hooknet/",
     license="LICENSE.txt",
     install_requires=[
-        "tensorflow_gpu>=2.3.0",
-        "opencv_python-headless>=4.4.0",
-        "scikit_image>=0.17.2",
         "numpy>=1.18.5",
-        "PyYAML>=5.3.1",
     ],
     long_description="HookNet: multi-resolution whole-slide image segmentation",
 )
