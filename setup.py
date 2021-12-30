8f06550959977b04190670c36055825956594994
martvanrijthoven <mart.vanrijthoven@gmail.com>
2021-12-30 09:58:17 +0100

    add config files to setup

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="0.0.3",
     author="Mart van Rijthoven",
     author_email="mart.vanrijthoven@gmail.com",
+    package_data={"": ["*.yml"]},
     packages=find_packages(exclude=("tests", "notebooks", "scripts", "os-level-virtualization", "docs")),
     url="http://pypi.python.org/pypi/hooknet/",
     license="LICENSE.txt",
