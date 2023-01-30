31defce2eb64a7b6d6d8cfad7a6b625ad213a9e8
martvanrijthoven <mart.vanrijthoven@gmail.com>
2023-01-30 12:02:07 +0100

    update training

--- hooknet/training/__main__.py
+++ hooknet/training/__main__.py
@@ -1,18 +1,7 @@
-import glob
-import os
 from pathlib import Path
 
 import click
-import numpy as np
-from creationism.utils import open_yaml
-from hooknet.configuration.config import create_hooknet
-from hooknet.training.metrics import (ConfusionMatrixAccumulator,
-                                      MetricAccumulater,
-                                      PredictionMetricAccumulator)
-from hooknet.training.tracker import WandbTracker
-from sklearn.metrics import log_loss
-from wholeslidedata.iterators import create_batch_iterator
-from hooknet.training.train import train, MODES
+from hooknet.training.trainer import Trainer
 
 
 @click.command()
@@ -24,57 +13,10 @@ from hooknet.training.train import train, MODES
 @click.option("--project", type=str, required=True)
 @click.option("--log_path", type=Path, required=True)
 def main(iterator_config, hooknet_config, epochs, steps, cpus, project, log_path):
-    tracker = WandbTracker(project=project, log_path=log_path)
+    trainer = Trainer(
+        iterator_config, hooknet_config, epochs, steps, cpus, project, log_path
+    )
+    trainer.train()
 
-    tracker.save(str(iterator_config))
-    tracker.save(str(hooknet_config))
-        
-    iterators = {
-        mode: create_batch_iterator(mode=mode, user_config=iterator_config, cpus=cpus, buffer_dtype=np.uint8)
-        for mode in MODES
-    }
-
-    hooknet = create_hooknet(hooknet_config, mode='training')
-    model_functions = {
-        "training": hooknet.train_on_batch,
-        "validation": hooknet.predict_on_batch,
-    }
-    weights_file = "./hooknet_weights.h5"
-
-
-    label_map = open_yaml(iterator_config)['hooknet']['default']['label_map']
-    print(label_map)
-    metrics = {
-        "training": [
-            MetricAccumulater("loss", "Loss+L2"),
-            MetricAccumulater("accuracy"),
-            MetricAccumulater("categorical_crossentropy", "Loss"),
-        ],
-        "validation": [
-            ConfusionMatrixAccumulator(
-                metric_names=("F1", "F1_Macro", "ACC_Macro"), label_map=label_map
-            ),
-            PredictionMetricAccumulator(log_loss, "Loss", argmax=False)
-        ],
-    }
-
-    print("Training...")
-    try:
-        train(
-            hooknet=hooknet,
-            epochs=epochs,
-            steps=steps,
-            iterators=iterators,
-            model_functions=model_functions,
-            metrics=metrics,
-            tracker=tracker,
-            weights_file=weights_file,
-        )
-    except Exception as exception:
-        # finish tracker
-        print("save wandb files...")
-        for file in glob.glob(os.path.join(log_path, "*.log")):
-            tracker.save(file)
-        raise exception
 
 main()
---
+++ hooknet/training/trainer.py
@@ -0,0 +1,131 @@
+import glob
+import os
+from pathlib import Path
+
+from dicfg.reader import _open_yaml_config
+from hooknet.configuration.config import create_hooknet
+from hooknet.training.metrics import (
+    ConfusionMatrixAccumulator,
+    MetricAccumulater,
+    PredictionMetricAccumulator,
+)
+from hooknet.training.tracker import WandbTracker
+from sklearn.metrics import log_loss
+from tqdm import tqdm
+from wholeslidedata.iterators import create_batch_iterator
+import numpy as np
+MODES = ["training", "validation"]
+
+
+class KerasUpdateLearningRate:
+    def __init__(
+        self,
+        model,
+        decay_rate=0.5,
+        decay_steps=[2, 4, 1000, 5000, 10_000, 50_000, 100_000],
+    ):
+        self._model = model
+        self._lr = self._model.optimizer.lr.numpy()
+        self._decay_rate = decay_rate
+        self._decay_steps = decay_steps
+        self._index = 0
+
+    def __call__(self):
+        if self._index in self._decay_steps:
+            self._lr *= self._decay_rate
+            self._model.optimizer.lr.assign(self._lr)
+            print(f"updated learning rate={self._lr}")
+        self._index += 1
+
+
+class Trainer:
+    def __init__(
+        self, iterator_config, hooknet_config, epochs, steps, cpus, project, log_path
+    ):
+        self._log_path = Path(log_path)
+        self._log_path.mkdir(parents=True, exists_ok=True)
+        self._tracker = WandbTracker(project=project, log_path=self._log_path)
+        self._epochs = epochs
+        self._steps = steps
+        self._tracker.save(str(iterator_config))
+        self._tracker.save(str(hooknet_config))
+
+        self._iterators = {
+            mode: create_batch_iterator(
+                mode=mode, user_config=iterator_config, cpus=cpus, buffer_dtype=np.uint8
+            )
+            for mode in MODES
+        }
+
+        self._hooknet = create_hooknet(hooknet_config, mode="training")
+
+        self._weights_file = log_path / "hooknet_weights.h5"
+
+        label_map = _open_yaml_config(iterator_config)["wholeslidedata"]["default"][
+            "labels"
+        ]
+
+        self._metrics = {
+            "training": [
+                MetricAccumulater("loss", "Loss+L2"),
+                MetricAccumulater("accuracy"),
+                MetricAccumulater("categorical_crossentropy", "Loss"),
+            ],
+            "validation": [
+                ConfusionMatrixAccumulator(
+                    metric_names=("F1", "F1_Macro", "ACC_Macro"), label_map=label_map
+                ),
+                PredictionMetricAccumulator(log_loss, "Loss", argmax=False),
+            ],
+        }
+
+    def train(self):
+        best_metric = None
+        update_learning_rate = KerasUpdateLearningRate(self._hooknet)
+        print("training labels", self._iterators["training"].dataset.labels.names)
+        print("validation labels", self._iterators["validation"].dataset.labels.names)
+        for _ in tqdm(range(self._epochs)):
+            for mode in MODES:
+                for _ in range(self._steps):
+                    x_batch, y_batch, _ = next(self._iterators[mode])
+                    x_batch = list(x_batch.transpose(1, 0, 2, 3, 4))
+                    y_batch = y_batch.transpose(1, 0, 2, 3, 4)[0]
+
+                    if mode == "training":
+                        out = self._hooknet.train_on_batch(
+                            x=x_batch, y=y_batch, return_dict=True
+                        )
+                    else:
+                        predictions = self._hooknet.predict_on_batch(
+                            x=x_batch, argmax=False
+                        )
+                        out = {"predictions": predictions, "y": y_batch}
+
+                    for metric in self._metrics[mode]:
+                        metric.update(**out)
+
+                    update_learning_rate()
+
+                metrics_data = {}
+                for metric in self._metrics[mode]:
+                    metrics_data.update(metric())
+
+                mode_metrics = {
+                    mode + "_" + name: value for name, value in metrics_data.items()
+                }
+                self._tracker.update(mode_metrics)
+
+                if mode == "validation":
+                    # check if model improved
+                    if best_metric is None or metrics_data["F1_Macro"] > best_metric:
+                        # update best metric
+                        best_metric = metrics_data["F1_Macro"]
+                        print("new best metric: ", best_metric)
+                        # tracker.update_best(best_metric)
+                        # save weights
+                        print(f"Saving weights to: {self._weights_file}")
+                        self._hooknet.save_weights(self._weights_file)
+        self._hooknet.save_weights("/home/user/last_model.h5")
+        print("save wandb files...")
+        for file in glob.glob(os.path.join(self._log_path, "*.log")):
+            self._tracker.save(file)
