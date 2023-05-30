ea93e4ea0cd78af6c9751d8f78a5323a6a0d19bb
martvanrijthoven <mart.vanrijthoven@gmail.com>
2023-05-30 08:53:03 +0200

    refactor

--- hooknet/training/torchstep.py
+++ hooknet/training/torchstep.py
@@ -1,45 +1,35 @@
 import numpy as np
 import torch
-from experimart.interoperability.torch.step import TorchStepIterator
+from experimart.interoperability.torch.step import (
+    TorchStepIterator,
+    TorchTrainingStepIterator,
+    TorchValidationStepIterator,
+)
 
 
-def _get_cuda_data(data, label):
-    data = np.transpose(data, (1, 0, 4, 2, 3))
-    label = np.transpose(label, (1, 0, 2, 3))
+class HookNetTorchStepIterator(TorchStepIterator):
+    def _get_data(self):
+        data, label, *_ = next(self._data_iterator)
+        data = np.transpose(data, (1, 0, 4, 2, 3))
+        label = np.transpose(label, (1, 0, 2, 3))[0]
 
-    data = torch.tensor(data, device="cuda").float() / 255.0
-    label = torch.tensor(label, device="cuda").long()
+        data = torch.tensor(data, device="cuda").float() / 255.0
+        label = torch.tensor(label, device="cuda").long()
+        return data, label
 
-    return data, label
-
-
-class HookNetTorchTrainingStepIterator(TorchStepIterator):
-    def steps(self):
-        self._model.train()
-        for _ in range(len(self)):
-            data, label, *_ = next(self._data_iterator)
-            data_cuda, label_cuda = _get_cuda_data(data, label)
-            self._components.optimizer.zero_grad()
-            output = self._model(*data_cuda)["out"]
-            loss = self._components.criterion(output, label_cuda[0])
-            loss.backward()
-            self._components.optimizer.step()
-            metrics = self.get_metrics(label_cuda[0], output)
-            metrics["learning_rate"] = self._components.scheduler.get_last_lr()[0]
-            yield {"loss": loss.item(), **metrics}
-        self._components.scheduler.step()
+    def _get_output(self, data):
+        return self._model(*data)["out"]
 
 
-class HookNetTorchValidationStepIterator(TorchStepIterator):
-    def steps(self):
-        self._model.eval()
-        with torch.no_grad():
-            for _ in range(len(self)):
-                data, label, *_ = next(self._data_iterator)
-                data_cuda, label_cuda = _get_cuda_data(data, label)
+class HookNetTorchTrainingStepIterator(
+    HookNetTorchStepIterator,
+    TorchTrainingStepIterator,
+):
+    ...
 
-                output = self._model(*data_cuda)
-                output = output["out"]
-                loss = self._components.criterion(output, label_cuda[0])
-                metrics = self.get_metrics(label_cuda[0], output)
-                yield {"loss": loss.item(), **metrics}
+
+class HookNetTorchValidationStepIterator(
+    HookNetTorchStepIterator,
+    TorchValidationStepIterator,
+):
+    ...
