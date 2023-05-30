ea102a78a4d4635231aa741ae6885c95458f8359
martvanrijthoven <mart.vanrijthoven@gmail.com>
2023-05-30 19:12:39 +0200

    refactor

--- scripts/torch.sh
+++ scripts/torch.sh
@@ -4,7 +4,7 @@
     experimart.default.log_path="'./'" \
     experimart.default.project="'hooknet-torch-test'" \
     experimart.default.data_settings.cpus=12 \
-    -c /home/user/pathology-hooknet/hooknet/configuration/torchtraining.yml \
+    -c /home/user/pathology-hooknet/hooknet/training/configuration/torchtraining.yml \
     -p wandb/tracker.yml \
     -p torch/training.yml \
     -p wholeslidedata/dataiterator.yml \ 
