0cb4ca8a7b0252924b94de92a7b3a1bd67a1741c
martvanrijthoven <mart.vanrijthoven@gmail.com>
2023-05-30 09:42:00 +0200

    add comment

--- hooknet/training/torchstep.py
+++ hooknet/training/torchstep.py
@@ -11,8 +11,8 @@ from experimart.interoperability.torch.step import (
 class HookNetTorchStepIterator(TorchMultiInputStepIterator, TorchStepIterator):
     def _get_data(self):
         data, label, *_ = next(self._data_iterator)
-        data = np.transpose(data, (1, 0, 4, 2, 3))
-        label = np.transpose(label, (1, 0, 2, 3))[0]
+        data = np.transpose(data, (1, 0, 4, 2, 3)) # inputs first
+        label = np.transpose(label, (1, 0, 2, 3))[0] # inputs first and high-res label only
         return convert_data_to_device(data, label, device=self._device)
 
 
