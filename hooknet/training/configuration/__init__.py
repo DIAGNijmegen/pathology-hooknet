90cf3f34aef1054b7bf5d986dbb96ce0fe33d55c
martvanrijthoven <mart.vanrijthoven@gmail.com>
2023-05-30 21:11:10 +0200

    restructure

--- hooknet/training/configuration/__init__.py
+++
@@ -1,4 +0,0 @@
-from pathlib import Path
-
-
-MAIN_CONFIG_PATH = Path(__file__).absolute().parent / "config.yml"
--- scripts/torchtraining.sh
+++ scripts/torchtraining.sh
@@ -1,10 +1,12 @@
  #! /bin/bash
  
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
  python3 -m experimart.training.train \
     experimart.default.log_path="'./'" \
     experimart.default.project="'hooknet-torch-test'" \
     experimart.default.data_settings.cpus=16 \
-    -c /home/user/pathology-hooknet/hooknet/training/configuration/torchtraining.yml \
+    -c $SCRIPT_DIR/configs/torchtraining.yml \
     -p wandb/tracker.yml \
     -p torch/training.yml \
     -p wholeslidedata/dataiterator.yml \ 
