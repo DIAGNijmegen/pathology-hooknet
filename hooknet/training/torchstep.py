16ab7522629d15451aa5053521eb92f881a319be
martvanrijthoven <mart.vanrijthoven@gmail.com>
2023-05-30 09:30:59 +0200

    refactor

--- hooknet/training/torchstep.py
+++ hooknet/training/torchstep.py
@@ -1,24 +1,19 @@
 import numpy as np
-import torch
+from experimart.interoperability.torch.io import convert_data_to_device
 from experimart.interoperability.torch.step import (
     TorchStepIterator,
+    TorchMultiInputStepIterator,
     TorchTrainingStepIterator,
     TorchValidationStepIterator,
 )
 
 
-class HookNetTorchStepIterator(TorchStepIterator):
+class HookNetTorchStepIterator(TorchMultiInputStepIterator, TorchStepIterator):
     def _get_data(self):
         data, label, *_ = next(self._data_iterator)
         data = np.transpose(data, (1, 0, 4, 2, 3))
         label = np.transpose(label, (1, 0, 2, 3))[0]
-
-        data = torch.tensor(data, device="cuda").float() / 255.0
-        label = torch.tensor(label, device="cuda").long()
-        return data, label
-
-    def _get_output(self, data):
-        return self._model(*data)["out"]
+        return convert_data_to_device(data, label, device="cuda")
 
 
 class HookNetTorchTrainingStepIterator(
